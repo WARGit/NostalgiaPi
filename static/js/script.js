f47901c1b8c8e8badab76f7e311efab6c5f9ad11
Wayne Robinson <wayne@sccm-consultants.com>
2025-09-25 19:10:06 +0100

    WIP web interface with Flask

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,8 @@
-document.addEventListener("DOMContentLoaded", function() {
-    console.log("JS loaded successfully!");
-});
+function toggleSidebar() {
+    const sidebar = document.getElementById("sidebar");
+    if (sidebar.style.width === "250px") {
+        sidebar.style.width = "0";
+    } else {
+        sidebar.style.width = "250px";
+    }
+}
